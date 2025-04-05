0760a35b9a7f35bc7e1f9b17b65517b9ec70913a
Akshat K <akshatk1001@gmail.com>
2025-04-04 17:25:44 -0700

    Co-authored-by: Justin Koida <Justin-Koida-SA@users.noreply.github.com> Co-authored-by: Justin Koida <Justin-Koida@users.noreply.github.com>

--- TaggingClubIdentity.py
+++ TaggingClubIdentity.py
@@ -4,19 +4,13 @@ import torch
 from sklearn import preprocessing
 
 DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
-
 model = SentenceTransformer("all-MiniLM-L6-v2")
-
 df = pd.read_csv("NicosScrapedData.csv")
-#Get a vector of the Club Names from the scraped data
-club_name = df["Club Name"].to_list()
-#get a list of the Descriptions of the club
-descriptions = df["Description Excerpt"].to_list()
 
 name_desc = df["Club Name"] + " " + df["Description Excerpt"]
+print(name_desc)
 
-
-
+# maybe add lgbtq tag
 all_identities = [
     "White European Italian",
     "Black African American",
@@ -45,7 +39,6 @@ all_identities = [
 ]
 
 all_embeddings = model.encode(name_desc, show_progress_bar=True, device=DEVICE)
-#descriptions_embeddings = model.encode(club_name, show_progress_bar=True, device=DEVICE)
 tags_embedding = model.encode(all_identities, show_progress_bar=True, device=DEVICE)
 
 similarity_matrix = model.similarity(all_embeddings, tags_embedding)
@@ -54,37 +47,65 @@ min_max_scaler = preprocessing.MinMaxScaler()
 scaled_similarity_matrix = min_max_scaler.fit_transform(similarity_matrix)
 
 sim_df = pd.DataFrame(scaled_similarity_matrix, columns= all_identities)
-sim_df['Club Name'] = club_name
+sim_df.insert(loc = 0, column = "Club Name", value = df["Club Name"])
 
-# race: threshold 0.6 and whatever is remaining take the highest value and set it to 1.0, Set the rest to 0.0
-# gender: use for loop to check for specific word. After, do thresholding with a value of 0.5. If both above or below 0.5, set to 1.0 for both. Otherwise, set larger to 1.0, and the other to 0.0. 
 # major: no threshold, no changing.
 
-
-
-# 0:4 Race
-# 5:6 gender
-# 7:8 greek life
-# 9:end major
+# 1:5 Race
+# 6:7 gender
+# 8:9 greek life
+# 10:end major
 
 race_thresh = 0.6
 race_cols = [ "White European Italian", "Black African American", "Native American", "Asian", "Hawaiian Pacific Islander"]
 
-# race: threshold 0.6 and whatever is remaining take the highest value and set it to 1.0, Set the rest to 0.0
-for index, row in sim_df.iterrows():    
-    race_list = row[0:4].to_list()
+for index, row in sim_df.iterrows():
+    # race: threshold 0.6 and whatever is remaining take the highest value and set it to 1.0, Set the rest to 0.0
+    race_list = row[1:5].to_list()
     maximum = max(race_list)
-    max_index = race_list.index(maximum)
     minimum = min(race_list)
+    max_index = race_list.index(maximum)
 
     for col in race_cols:
-        sim_df.loc[index, col] = 0.0
+        sim_df.loc[index, col] = 0.0 # MIGHT have to set to 0.5 
 
     if minimum >= race_thresh:
         for col in race_cols:
-            sim_df.loc[index, col] = 1.0        
+            sim_df.loc[index, col] = 1.0
             
     elif maximum > race_thresh:
         sim_df.loc[index, race_cols[max_index]] = 1.0
+
+    # gender: use for loop to check for specific word. After, do thresholding with a value of 0.5. If both above or below 0.5, set to 1.0 for both. Otherwise, set larger to 1.0, and the other to 0.0. 
+    women_list = ["woman", "woman's", "women", "women's", "womens", "sisterhood", "sister", "sisters", "sorority"]
+    men_list = ["man", "man's", "men", "men's", "mens", "brotherhood", "brother", "brothers"]
+
+    club_name = sim_df.loc[index, "Club Name"].lower()
+
+    # O(n^2) lmaooooo
+    found = None
+
+    for word in club_name.split():
+        if word in women_list:
+            found = "woman"
+            break
+        elif word in men_list:
+            found = "man"
+            break
+
+    if found == "man":
+        sim_df.loc[index, "woman women"] = 0.0
+        sim_df.loc[index, "man men"] = 1.0
+    elif found == "woman":
+        sim_df.loc[index, "woman women"] = 1.0
+        sim_df.loc[index, "man men"] = 0.0
+    else:
+        # do thresholding with a value of 0.5. If both above or below 0.5, set to 1.0 for both. Otherwise, set larger to 1.0, and the other to 0.0. 
+        # TODO. IMMA DO IT ONCE IM AT THE MEETING AREA. LAUNDRY JUST FINSIHED HEADING OVER NOW.
+
+        sim_df.loc[index, "woman women"] = 1.0 # why are we seeting to 1 here? just for now. imma change it in a sec
+        sim_df.loc[index, "man men"] = 1.0
+    
+    
 
 sim_df.to_csv('IdentityScored.csv', index=False)
